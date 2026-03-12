ea0bcd30e78a9bfc86771757ae8b2b1b297ac904
kssim2025 <hgoodi150@gmail.com>
2026-03-12 13:10:35 +0900

    versiontest1
    java8 version test

--- scripts/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user
-#nohup ./main > nohup.out 2>&1 &
-echo "cicd test ok" >> test.txt
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-fuser -k 8080/tcp && echo "Stop Server" || echo "Not Running"
+# 실제 종료 로직은 start_home.sh의 변경 감지 로직 안으로 이동했으므로 
+# 여기서는 로그만 출력합니다.
+echo "Checking update status in Start step..."
+exit 0
